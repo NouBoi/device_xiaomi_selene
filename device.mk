564925d4847b6e15036bf79bca67a1a72d338fd4
bengris32 <bengris32@protonmail.ch>
2022-09-13 18:07:32 +0100

    selene: Build required Camera interfaces

--- device.mk
+++ device.mk
@@ -124,24 +124,15 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
-    android.hardware.camera.provider@2.4 \
-    android.hardware.camera.provider@2.4.vendor \
-    android.hardware.camera.provider@2.5 \
-    android.hardware.camera.provider@2.5.vendor \
-    android.hardware.camera.provider@2.6 \
-    android.hardware.camera.provider@2.6.vendor \
-    android.hardware.camera.device@3.2 \
-    android.hardware.camera.device@3.2.vendor \
-    android.hardware.camera.device@3.3 \
+    android.hardware.camera.common@1.0.vendor \
     android.hardware.camera.device@3.3.vendor \
-    android.hardware.camera.device@3.4 \
     android.hardware.camera.device@3.4.vendor \
-    android.hardware.camera.device@3.5 \
     android.hardware.camera.device@3.5.vendor \
-    android.hardware.camera.device@3.6 \
-    android.hardware.camera.device@3.6.vendor
-    
+    android.hardware.camera.device@3.6.vendor \
+    android.hardware.camera.provider@2.4.vendor \
+    android.hardware.camera.provider@2.5.vendor \
+    android.hardware.camera.provider@2.6.vendor \
+
 # Disable Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
